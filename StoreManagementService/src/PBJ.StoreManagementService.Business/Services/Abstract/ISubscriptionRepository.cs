d4c7b93df9af49adb1134c976fc78da8c63ad0f4
kalachck <kalachck@gmail.com>
2023-07-19 10:20:38 +0300

    add service interfaces

---
+++ StoreManagementService/src/PBJ.StoreManagementService.Business/Services/Abstract/ISubscriptionRepository.cs
@@ -0,0 +1,19 @@
+﻿using PBJ.StoreManagementService.Business.Dtos;
+
+namespace PBJ.StoreManagementService.Business.Services.Abstract
+{
+    public interface ISubscriptionRepository
+    {
+        Task<List<SubscriptionDto>> GetAmountAsync(int amount);
+
+        Task<List<SubscriptionDto>> GetUserSubscriptionsAsync(int userId, int amount);
+
+        Task<SubscriptionDto> GetAsync(int id);
+
+        Task<bool> CreateAsync(SubscriptionDto subscriptionDto);
+
+        Task<bool> UpdateAsync(int id, SubscriptionDto subscriptionDto);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ StoreManagementService/src/PBJ.StoreManagementService.Business/Services/Abstract/IUserSubscriptionService.cs
@@ -0,0 +1,17 @@
+﻿using PBJ.StoreManagementService.Business.Dtos;
+
+namespace PBJ.StoreManagementService.Business.Services.Abstract
+{
+    public interface IUserSubscriptionService
+    {
+        Task<List<UserSubscriptionDto>> GetAmountAsync(int amount);
+
+        Task<UserSubscriptionDto> GetAsync(int id);
+
+        Task<bool> CreateAsync(UserSubscriptionDto userSubscriptionDto);
+
+        Task<bool> UpdateAsync(int id, UserSubscriptionDto userSubscriptionDto);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
