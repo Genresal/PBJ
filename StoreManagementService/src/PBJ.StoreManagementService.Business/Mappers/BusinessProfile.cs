dd4f2a80a9fcf71b7544d4c66c591ba26855f4df
kalachck <kalachck@gmail.com>
2023-07-19 10:37:15 +0300

    add dto to entities mappings

---
+++ StoreManagementService/src/PBJ.StoreManagementService.Business/Mappers/BusinessProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PBJ.StoreManagementService.Business.Dtos;
+using PBJ.StoreManagementService.DataAccess.Entities;
+
+namespace PBJ.StoreManagementService.Business.Mappers
+{
+    public class BusinessProfile : Profile
+    {
+        public BusinessProfile()
+        {
+            CreateMap<UserDto, User>().ReverseMap();
+            CreateMap<CommentDto, Comment>().ReverseMap();
+            CreateMap<PostDto, Post>().ReverseMap();
+            CreateMap<FollowingDto, Following>().ReverseMap();
+            CreateMap<SubscriptionDto, Subscription>().ReverseMap();
+            CreateMap<UserSubscriptionDto, UserSubscription>().ReverseMap();
+            CreateMap<UserFollowingDto, UserFollowing>().ReverseMap();
+        }
+    }
+}
