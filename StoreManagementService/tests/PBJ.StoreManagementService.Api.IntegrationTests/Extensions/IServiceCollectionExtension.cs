d06b3561167a0e7f11ba47f9579913f929ad3d6b
kalachck <kalachck@gmail.com>
2023-07-21 10:41:32 +0300

    add tests structure

---
+++ StoreManagementService/tests/PBJ.StoreManagementService.Api.IntegrationTests/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PBJ.StoreManagementService.DataAccess.Context;
+
+namespace PBJ.StoreManagementService.Api.IntegrationTests.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static void AddInMemoryDatabase(this IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(x =>
+                x.ServiceType == typeof(DbContextOptions<DatabaseContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseInMemoryDatabase("sms");
+            });
+        }
+    }
+}
---
+++ StoreManagementService/tests/PBJ.StoreManagementService.Api.IntegrationTests/Extensions/IServiceProviderExtension.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.DependencyInjection;
+using PBJ.StoreManagementService.DataAccess.Entities.Abstract;
+using PBJ.StoreManagementService.DataAccess.Repositories;
+
+namespace PBJ.StoreManagementService.Api.IntegrationTests.Extensions
+{
+    public static class IServiceProviderExtension
+    {
+        private static readonly Fixture _fixture;
+
+        static IServiceProviderExtension()
+        {
+            _fixture = new Fixture();
+        }
+
+        public static async Task SeedDataAsync<TRepository, TEntity>(this IServiceProvider provider)
+        where TEntity : BaseEntity
+        where TRepository : BaseRepository<TEntity>
+        {
+            var repository = provider.GetService<TRepository>();
+
+            await repository.CreateAsync(_fixture.Create<TEntity>());
+            await repository.CreateAsync(_fixture.Create<TEntity>());
+        }
+    }
+}
