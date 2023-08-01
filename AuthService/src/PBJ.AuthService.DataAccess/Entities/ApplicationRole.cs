c3edaea536ba2f2ac3489b47f66c18f3729a2eb3
kalachck <kalachck@gmail.com>
2023-08-01 16:35:37 +0300

    Add data access layer

---
+++ AuthService/src/PBJ.AuthService.DataAccess/Entities/ApplicationRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PBJ.AuthService.DataAccess.Entities
+{
+    public class ApplicationRole : IdentityRole<int>
+    {
+        public ApplicationRole() : base()
+        { }
+
+        public ApplicationRole(string roleName) : base(roleName)
+        { }
+    }
+}
---
+++ AuthService/src/PBJ.AuthService.DataAccess/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PBJ.AuthService.DataAccess.Context;
+
+namespace PBJ.AuthService.DataAccess.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        }
+    }
+}
