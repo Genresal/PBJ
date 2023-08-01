fa5fe88df9a6b94a8429e2f851c891521b93dfee
Сергей Калачев <73075156+kalachck@users.noreply.github.com>
2023-08-01 12:30:34 +0300

    Sms/fix/app fix (#30)
    * fix post and comments update logic
    
    * add missing files

---
+++ StoreManagementService/src/PBJ.StoreManagementService.Api/Validators/UpdatePostValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using PBJ.StoreManagementService.Models.Post;
+
+namespace PBJ.StoreManagementService.Api.Validators
+{
+    public class UpdatePostValidator : AbstractValidator<UpdatePostRequestModel>
+    {
+        public UpdatePostValidator()
+        {
+            RuleFor(x => x.Content).NotEmpty().NotNull().MinimumLength(30);
+        }
+    }
+}
