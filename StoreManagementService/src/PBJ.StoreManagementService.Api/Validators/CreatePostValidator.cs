7292f0b2a8195dff3d47340a2d8aaea560f37952
kalachck <kalachck@gmail.com>
2023-08-01 12:29:20 +0300

    add missing files

---
+++ StoreManagementService/src/PBJ.StoreManagementService.Api/Validators/CreatePostValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PBJ.StoreManagementService.Models.Post;
+
+namespace PBJ.StoreManagementService.Api.Validators
+{
+    public class CreatePostValidator : AbstractValidator<CreatePostRequestModel>
+    {
+        public CreatePostValidator()
+        {
+            RuleFor(x => x.Content).NotEmpty().NotNull().MinimumLength(30);
+            RuleFor(x => x.UserId).Must(x => x > 0);
+        }
+    }
+}
