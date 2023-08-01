74f0fc8564f867cb56c330142649a646c8dbdd12
kalachck <kalachck@gmail.com>
2023-08-01 16:10:31 +0300

    setup authorization service structure

---
+++ AuthService/src/PBJ.AuthService.Api/Program.cs
@@ -0,0 +1,36 @@
+using PBJ.AuthService.DataAccess.Extensions;
+
+namespace PBJ.AuthService.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDatabase(builder.Configuration);
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseAuthentication();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
