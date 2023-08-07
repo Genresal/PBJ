a52fd7f7ea215ee8a86a2160f4bbfc8740e85502
kalachck <kalachck@gmail.com>
2023-08-07 13:00:20 +0300

    change directories

--- AuthService/src/PBJ.AuthService.Api/Extensions/IServiceCollectionExtensions.cs
+++ AuthService/src/PBJ.AuthService.Api/Extensions/IServiceCollectionExtensions.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using PBJ.AuthService.Api.RequestModels;
+using PBJ.AuthService.Api.Validators;
+using PBJ.AuthService.Business.Services;
 using PBJ.AuthService.DataAccess.Context;
 using PBJ.AuthService.DataAccess.Entities;
 
@@ -10,12 +16,14 @@ namespace PBJ.AuthService.Api.Extensions
         public static void SetupIdentity(this IServiceCollection services)
         {
             services.AddIdentity<AuthUser, AuthRole>(options =>
-                {
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequiredLength = 4;
-                })
+            {
+                options.User.RequireUniqueEmail = true;
+
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 4;
+            })
                 .AddEntityFrameworkStores<AuthDbContext>()
                 .AddDefaultTokenProviders();
         }
@@ -26,19 +34,20 @@ namespace PBJ.AuthService.Api.Extensions
 
             services.AddIdentityServer(options =>
                 {
-                    options.UserInteraction.LoginUrl = "/auth/sign-in";
+                    options.UserInteraction.LoginUrl = "/auth/login";
 
                     options.Events.RaiseErrorEvents = true;
                     options.Events.RaiseInformationEvents = true;
                     options.Events.RaiseFailureEvents = true;
                     options.Events.RaiseSuccessEvents = true;
                 })
+                .AddAspNetIdentity<AuthUser>()
+                .AddProfileService<ProfileService>()
                 .AddConfigurationStore<AuthDbContext>(options =>
                 {
                     options.ConfigureDbContext = builder =>
                     {
                         builder.UseSqlServer(connectionString);
-                        builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                     };
                 })
                 .AddOperationalStore<AuthDbContext>(options =>
@@ -46,12 +55,17 @@ namespace PBJ.AuthService.Api.Extensions
                     options.ConfigureDbContext = builder =>
                     {
                         builder.UseSqlServer(connectionString);
-                        builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                         options.EnableTokenCleanup = true;
                         options.TokenCleanupInterval = 3600;
                     };
                 })
                 .AddDeveloperSigningCredential();
+        }
+
+        public static void AddValidations(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<LoginRequestModel>, LoginValidator>();
+            services.AddScoped<IValidator<RegisterRequestModel>, RegisterValidator>();
         }
     }
 }
