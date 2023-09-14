99d3d15057dcb49124358bcd91fdaa4b90f86e2b
Vlad <infin95@gmail.com>
2023-09-14 11:44:12 +0300

    fix bugs

--- StoreManagementService/src/PBJ.StoreManagementService.Business/Producers/MessageProducer.cs
+++ StoreManagementService/src/PBJ.StoreManagementService.Business/Producers/MessageProducer.cs
@@ -1,21 +1,21 @@
-﻿//using MassTransit;
-//using PBJ.Shared.QueueContext.MailDtos;
-//using PBJ.StoreManagementService.Business.Producers.Abstract;
+﻿using MassTransit;
+using PBJ.Shared.QueueContext.MailDtos;
+using PBJ.StoreManagementService.Business.Producers.Abstract;
 
-//namespace PBJ.StoreManagementService.Business.Producers
-//{
-//    public class MessageProducer : IMessageProducer
-//    {
-//        private readonly IPublishEndpoint _publishEndpoint;
+namespace PBJ.StoreManagementService.Business.Producers
+{
+    public class MessageProducer : IMessageProducer
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
 
-//        public MessageProducer(IPublishEndpoint publishEndpoint)
-//        {
-//            _publishEndpoint = publishEndpoint;
-//        }
+        public MessageProducer(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
 
-//        public async Task PublicCommentMessageAsync(string email, string message)
-//        {
-//            await _publishEndpoint.Publish(new MailDto(email, message));
-//        }
-//    }
-//}
+        public async Task PublicCommentMessageAsync(string email, string message)
+        {
+            await _publishEndpoint.Publish(new MailDto(email, message));
+        }
+    }
+}
